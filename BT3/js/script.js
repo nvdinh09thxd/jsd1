5f3c9eab47cfa174b64b2510f229392cc67f0a68
nvdinh185 <nvdinh0766777123@gmail.com>
2022-06-24 16:33:30 +0700

    x

--- BT3/js/script.js
+++ BT3/js/script.js
@@ -1,22 +1,22 @@
-function getName(){
+function getName() {
 	var name = DomID("hoten");
-	alert("Ho ten la: "+name); 
+	alert("Ho ten la: " + name);
 }
-function getAge(){
+function getAge() {
 	var age = DomID("tuoi");
-	alert("Tuoi la: "+age);
+	alert("Tuoi la: " + age);
 }
-function getHeight(){
+function getHeight() {
 	var height = DomID("chieucao");
-	alert("Chieu cao la: "+height); 
+	alert("Chieu cao la: " + height);
 }
-function getInfo(){
+function getInfo() {
 	var name = DomID("hoten");
 	var age = DomID("tuoi");
 	var height = DomID("chieucao");
-	alert("Ten la: "+name+"\nTuoi la: "+age+"\nChieu cao la: "+height); 
+	alert("Ten la: " + name + "\nTuoi la: " + age + "\nChieu cao la: " + height);
 }
-function DomID(id){
+function DomID(id) {
 	var element = document.getElementById(id).value;
 	return element;
 }
