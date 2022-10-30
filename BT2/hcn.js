825257565d01b26d0f9ff8d07a6c31e503f4f15a
nvdinh185 <nvdinh0766777123@gmail.com>
2022-10-30 17:33:05 +0700

    x

--- BT2/hcn.js
+++ BT2/hcn.js
@@ -1,12 +1,13 @@
 function tinhDienTich() {
 	var chieudai = document.getElementById("dai").value;
-	var chieurong = document.getElementById("rong").value
+	var chieurong = document.getElementById("rong").value;
 	if (chieudai == "" || chieurong == "") alert("Vui long nhap kich thuoc!");
 	else {
 		chieudai = parseInt(chieudai);
 		chieurong = parseInt(chieurong);
-		if (!chieudai || !chieurong || chieurong >= chieudai) alert("Nhap kich thuoc sai, vui long nhap lai!");
-		else {
+		if (!chieudai || !chieurong || chieurong >= chieudai) {
+			alert("Nhap kich thuoc sai, vui long nhap lai!");
+		} else {
 			var dientich = chieudai * chieurong;
 			var ketqua = confirm("Dien tich hinh chu nhat la: " + dientich);
 			if (ketqua) {
